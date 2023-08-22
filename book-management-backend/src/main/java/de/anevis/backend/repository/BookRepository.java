3381472c1b16002b4ba2b52f1e7f2389ed86a92a
cn1adil24 <shehmeeradil123@gmail.com>
2023-08-22 13:16:55 +0200

    Revert back to JpaRepository

--- book-management-backend/src/main/java/de/anevis/backend/repository/BookRepository.java
+++ book-management-backend/src/main/java/de/anevis/backend/repository/BookRepository.java
@@ -4,9 +4,9 @@ import de.anevis.backend.domain.Book;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 	
 	Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 	
