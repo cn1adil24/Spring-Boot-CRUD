1d97f2a13b57d5db08cc1f9972386f0f2f1febd1
cn1adil24 <shehmeeradil123@gmail.com>
2023-08-30 16:51:23 +0200

    Pagination in UI

--- book-management-frontend/src/App.tsx
+++ book-management-frontend/src/App.tsx
@@ -4,19 +4,22 @@ import BookList from './components/BookList';
 import BookModal from './components/BookModal';
 import FilterTextBox from './components/FilterTextBox';
 import Buttons from './components/Buttons';
-import { Book, AddBookModel, Cover, BookModel } from './models';
+import { Book, AddBookModel, BookModel } from './models';
 import { Page } from './models';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Pagination from './components/Pagination';
 
 const App: React.FC = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const [selectedBook, setSelectedBook] = useState<Book | null>(null);
   const [filterText, setFilterText] = useState('');
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
-    populateList(setBooks, filterText);
+    populateList(0, filterText);
   }, []);
 
   const handleRowClick = (book: Book) => {
@@ -27,7 +30,7 @@ const App: React.FC = () => {
     const url = "http://localhost:8080/books/" + id;
     axios.delete(url)
       .then(() => {
-        populateList(setBooks, filterText);
+        populateList(currentPage, filterText);
         toast.success("Successfully deleted book.");
       })
       .catch((error) => {
@@ -58,7 +61,7 @@ const App: React.FC = () => {
   };
 
   const handleApplyFilter = () => {
-    populateList(setBooks, filterText);
+    populateList(0, filterText);
   };
 
   const handleAddBook = (newBookModel: AddBookModel) => {
@@ -76,16 +79,40 @@ const App: React.FC = () => {
       })
   };
 
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    populateList(page - 1, filterText);
+  };
+
   return (
     <div className="container mt-4">
       <h1 className="fancy-title text-center">Book Management</h1>
       <FilterTextBox filterText={filterText} onFilterChange={handleFilterChange}/>
       <Buttons onAdd={handleAddBook} onApply={handleApplyFilter} />
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
       <BookList books={books} onRowClick={handleRowClick} onDelete={handleDelete} />
       <BookModal book={selectedBook} onClose={() => setSelectedBook(null)} onEdit={handleEditBook} />
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
       <ToastContainer />
     </div>
   );
+
+  function populateList(page: number, filterText: string) {
+    let url: string = `http://localhost:8080/books?page=${page}&size=10`;
+    if (filterText !== null && filterText !== ''){
+      url = url + `&title=${filterText}`;
+    }
+    axios.get<Page>(url)
+      .then((response) => {
+        setBooks(response.data.content);
+        setCurrentPage(page);
+        setTotalPages(response.data.totalPages);
+        console.log("Books fetched.");
+      })
+      .catch(error => {
+        console.error("Error loading books:", error);
+      });
+  }
 };
 
 function getBookFromAddBookModel(newBookModel: AddBookModel): BookModel {
@@ -100,21 +127,6 @@ function getBookFromAddBookModel(newBookModel: AddBookModel): BookModel {
       S: addSizeToCoverURL(newBookModel.cover_url, 'S'),
     }
   };
-}
-
-function populateList(setBooks: React.Dispatch<React.SetStateAction<Book[]>>, filterText: string) {
-  let url: string = "http://localhost:8080/books?page=0&size=10";
-  if (filterText !== null && filterText !== ''){
-    url = url + `&title=${filterText}`;
-  }
-  axios.get<Page>(url)
-    .then((response) => {
-      setBooks(response.data.content);
-      console.log("Books fetched.");
-    })
-    .catch(error => {
-      console.error("Error loading books:", error);
-    });
 }
 
 function addSizeToCoverURL(url: string, letter: string) {
---
+++ book-management-frontend/src/components/Pagination.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+  const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+
+  return (
+    <nav>
+      <ul className="pagination justify-content-center">
+        {pages.map(page => (
+          <li key={page} className={`page-item ${currentPage + 1 === page ? 'active' : ''}`}>
+            <button className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
